be04f1451c56bfc4784182b9a4d4dd4912e0bde8
Jason Woo <jaswoo73@gmail.com>
2024-02-19 19:06:35 +0800

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import aboutSvg from "../assets/about.svg";
+import SectionTitle from "./SectionTitle";
+
+const About = () => {
+  return (
+    <section className="bg-white py-20" id="about">
+      <div className="align-element grid md:grid-cols-2 items-center gap-16">
+        <img src={aboutSvg} alt="About Image" className="w-full h-64" />
+        <article>
+          <SectionTitle text="code and coffee" />
+          <p className="text-slate-600 mt-8 leading-loose">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A minus
+            fugiat in id explicabo, et aliquam cumque ipsam qui architecto
+            itaque odit eius possimus ratione quo ea iusto vitae veritatis?
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
+export default About;
